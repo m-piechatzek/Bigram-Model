a0663f07c8fcbdebbc4c5d074b4ea42bcdb0ffe4
MonikaP <piechatzek@hotmail.com>
2017-10-14 21:38:21 -0700

    Summed up most frequent bigrams of V words, in a nested dict and printed out in a matrix

--- project1.py
+++ project1.py
@@ -13,6 +13,7 @@ import os
 from tabulate import tabulate
 import testingTrivialTokenizer as tt
 import re
+import pandas as pd
 
 # function to get user input for an existing directory
 def directory_input(message):
@@ -63,13 +64,30 @@ for subdir, dirs, files in os.walk(rootdir):
         # gather all bigrams
         bgrms = list(nltk.bigrams(tokenized_file))
         # does a comparison of the first word in the bigram to the V list
-        first_run_bgrms = list(set([b for b in bgrms if b[0] in v_words]))
+        first_run_bgrms = list([b for b in bgrms if b[0] in v_words])
         # does a second comparison to the second word in the bigram to the V list
-        clean_bigram = list(set([b for b in first_run_bgrms if b[1] in v_words]))
-        print(clean_bigram)
+        clean_bigram = list([b for b in first_run_bgrms if b[1] in v_words])
+        set_clean_bg = list(set([b for b in first_run_bgrms if b[1] in v_words]))
 
+        # create the dictionary with all values set to 0. ex big_dict[word1][word1]=0,big_dict[word1][word2] = 0
+        big_dict = {}
+        for v in v_words:
+            big_dict[v]={}
+            for t in v_words:
+                big_dict[v][t]= 0
+                
+        # adds up all the instances of bigrams in the dictionary
+        for bg in clean_bigram:
+            for k,v in big_dict.items():
+                for k1,v1 in v.items():
+                    # compares bigrams to dictionary keys to find how many times the bigram appeared
+                    if bg[0]== k and bg[1] == k1:
+                        big_dict[k][k1]= big_dict[k][k1] + 1
 
         # create a VxV matrix initialized with VxV zeroes
         bigram_matrix = [[0 for i in range(0,V)] for j in range (0,V)]
-
         print_matrix(bigram_matrix, v_words)
+
+        # Added my own table using Pandas
+        df = pd.DataFrame(big_dict).T
+        print(df)
