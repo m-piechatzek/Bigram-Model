7383dc4e6fdf4d9cd93b6b79a07b75da7d66f135
Fran Ramunno <franramunno@gmail.com>
2017-10-15 20:00:47 -0700

    Update project1.py

--- project1.py
+++ project1.py
@@ -10,6 +10,7 @@ from tabulate import tabulate
 import testingTrivialTokenizer as tt
 import re
 import pandas as pd
+import json
 
 
 # function to get user input for an existing directory
@@ -24,22 +25,20 @@ def directory_input(message):
 # function to print a nxn matrix using tabulate
 def print_matrix(bigram_matrix, v_words):
     i = 0
-    for row in bigram_matrix:
-        row.insert(0, v_words[i])
-        i = i + 1
-    print(tabulate(bigram_matrix, headers=v_words))
+    print(tabulate(bigram_matrix, headers=v_words, tablefmt='orgtbl'))
     print('')
 
 
 rootdir = directory_input("Enter a directory path: ")
 
 # V will be used as the top V most frequency counts
-V = 10
+V = 4000
 
 # process each file in given directory
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
-        print("Processing ", file, "....")
+        print("")
+        print("--           FOR FILE: ", file, "        --")
 
         # tokenize the file using the simplistic word tokenizer
         tokenized_file = tt.trivialTokenizer(open(rootdir + "/" + file).read())
@@ -87,15 +86,48 @@ for subdir, dirs, files in os.walk(rootdir):
                     if bg[0] == k and bg[1] == k1:
                         big_dict[k][k1] = float(big_dict[k][k1] + 1.0)
 
+        # print(json.dumps(big_dict, indent=4))
+
         # create a VxV matrix initialized with VxV zeroes
         bigram_matrix = [[0 for i in range(0, V)] for j in range(0, V)]
+
+        i = 0
+        v_words = []
+        for k,v in big_dict.items():
+            v_words.append(k)
+            j = 0
+            for k1, v1 in v.items():
+                bigram_matrix[i][j] = v1
+                j = j + 1
+            i = i + 1
+
+        most_common_counts = []
+
+        for word in v_words:
+            for a,b in most_common:
+                if a == word:
+                    most_common_counts.append(b)
+
+        print('')
+        print('--------------------------------------------------------------------------------------')
+        print('--                               POPULATED BIGRAM MATRIX                            --')
+        print('--------------------------------------------------------------------------------------')
+        print('')
+
         print_matrix(bigram_matrix, v_words)
+        print('')
 
         # Added my own table using Pandas
-        df = pd.DataFrame(big_dict).T
-        print(df)
+        #print(" DICTIONARY AFTER POPULATION")
+        #df = pd.DataFrame(big_dict).T
+        #print(df)
 
         # -- Perform Laplace smoothing -- #
+
+        print('--------------------------------------------------------------------------------------')
+        print("--                             PERFORM LAPLACE SMOOTHING                            --")
+        print('--------------------------------------------------------------------------------------')
+        print('')
 
         # first for bigrams
         # let L = number of bigrams with only 1 to 9 occurences
@@ -107,14 +139,66 @@ for subdir, dirs, files in os.walk(rootdir):
                 if v1 > 0 and v1 < 10:
                     l = l + 1
 
-        # increase each bigram count by 1 / L
-        for k,v in big_dict.items():
-            for k1, v1 in v.items():
-                v1 = float(v1)
-                v1 = v1 + 1/l
+        # this will hold the smoothed bigram values for presentation
+        smoothed_bigram_counts = [[0 for i in range(0, V)] for j in range(0, V)]
 
-        df = pd.DataFrame(big_dict).T
+        # increase each bigram count by 1 / L, put into a matrix
+        i = 0
+        for k,v in big_dict.items():
+            j = 0
+            for k1, v1 in v.items():
+                smoothed_bigram_counts[i][j] = float(format((v1 + 1/l), '.5f'))
+                j = j + 1
+            i = i + 1
+
+        print("AFTER BIGRAM LAPLACE SMOOTHING:")
         print('')
-        print('AFTER LAPLACE SMOOTHING: (BIGRAMS)')
-        print(df)
+        print_matrix(smoothed_bigram_counts,v_words)
+        print('')
+
+        # now perform Laplace smoothing for unigrams
+
+        print('UNIGRAMS BEFORE SMOOTHING:')
+        print('')
+        print(most_common_counts)
+        print('')
+
+        # this will hold the unigram count values after laplace smoothing
+        most_common_counts_smoothed = []
+
+        for val in most_common_counts:
+            most_common_counts_smoothed.append(float(format((val + (V/l)),'.5f')))
+
+        print("UNIGRAMS AFTER SMOOTHING")
+        print('')
+        print(most_common_counts_smoothed)
+        print('')
+
+        # -- GET MAXIMUM LIKELIHOOD ESTIMATE PROBABILITIES -- #
+        # bigram count / unigram count
+
+        probability_matrix = [[0 for i in range(0, V)] for j in range(0, V)]
+
+
+        for row in smoothed_bigram_counts:
+            i = 0
+            for col in row:
+                j = 0
+                probability_matrix[i][j] = float(format((col / most_common_counts_smoothed[i]),'.5f'))
+                j = j + 1
+            i = i + 1
+
+
+        print('-----------------------------------------------------------------------------------------------------------')
+        print("--                             THE MAXIMUM LIKELIHOOD ESTIMATE PROBABILITIES                             --")
+        print('-----------------------------------------------------------------------------------------------------------')
+        print('')
+
+        print_matrix(probability_matrix,v_words)
+
+        # -- PERFORM SANITY CHECK -- #
+
+        print('--------------------------------------------------------------------------------------')
+        print('--                              PERFORM A SANITY CHECK                              --')
+        print('--------------------------------------------------------------------------------------')
         print('')
